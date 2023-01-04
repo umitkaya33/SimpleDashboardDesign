cb074c2d7ae5582d5466da1bb20dbceeb34bf4a2
Ümit Kaya <33umitkaya@gmail.com>
2023-01-04 14:03:50 +0300

    menuToggle css & js code complete

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+//menuToggle
+let toggle = document.querySelector('.toggle');
+let navigation = document.querySelector('.navigation');
+let main = document.querySelector('.main');
+toggle.onclick = function () {
+    navigation.classList.toggle('active');
+    main.classList.toggle('active');
+
+}
+
+// add hovered class in selected list item
 let list = document.querySelectorAll('.navigation li');
 function activeLink() {
     list.forEach((item) => item.classList.remove('hovered'));
